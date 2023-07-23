6e87ca8f4acb30ef286e62e16aab3be4208f262b
정혜인 <jeonghyein@jeonghyein-ui-MacBookPro.local>
2023-07-23 23:45:13 +0900

    feat : 유효성 검사 이전 로직 구현

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,41 +1,37 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { Movie } from './entities/Movie.entity';
 
 @Controller('movies')
 export class MoviesController {
-    @Get()
-    getAll(){
-        return "This will return all movies';"
-    }
+    constructor(private readonly moviesService: MoviesService){}
 
-    @Get("search")
-    search(@Query("year") searchingYear:string){
-        return `We are searching for a movie made after: ${searchingYear}`;
+    @Get()
+    getAll(): Movie[]{
+        return this.moviesService.getAll();
     }
     
     @Get("/:id") //id별
-    GetOne(@Param('id')  movieId: string){
-        return `This will return one movie with the id: ${movieId}`;
+    getOne(@Param('id')  movieId: string): Movie{
+        //타입을 지정할 수 있으며 현재 Movie를 리턴하는 것
+        return this.moviesService.getOne(movieId);
     }
 
     @Post()//post는 기본적으로 movie를 '생성'하기 위함
     create(@Body() movieData){
-        console.log(movieData);
-        return movieData
+        return this.moviesService.create(movieData);
 ;    }
 
     @Delete('/:id')
     remove(@Param('id') movieId:string){
-        return "This will delete a movie";
+        return this.moviesService.deleteOne(movieId);
     }
 
     // @Patch() 리소스의 일부만 업데이트
     // 우리가 보낼 데이터의 오브젝트를 리턴함
     @Patch('/:id')
     patch(@Param('id') movieId: string, @Body() updateData){
-        return {
-            updateData: movieId,
-            ...updateData,
-        };
+        return this.moviesService.update(movieId, updateData);
     }
 
 }
