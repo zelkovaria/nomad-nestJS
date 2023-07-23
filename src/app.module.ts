9a0a1fdbfa4ce5756cb58bdea835a559c492974c
정혜인 <jeonghyein@jeonghyein-ui-MacBookPro.local>
2023-07-23 22:24:54 +0900

    feat : movies.service.ts 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MoviesController } from './movies/movies.controller';
+import { MoviesService } from './movies/movies.service';
 
 @Module({
   imports: [],
   controllers: [MoviesController],
-  providers: [],
+  providers: [MoviesService],
 })
 export class AppModule {}
--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
@@ -6,15 +6,22 @@ export class MoviesController {
     getAll(){
         return "This will return all movies';"
     }
+
+    @Get("search")
+    search(@Query("year") searchingYear:string){
+        return `We are searching for a movie made after: ${searchingYear}`;
+    }
+    
     @Get("/:id") //id별
     GetOne(@Param('id')  movieId: string){
         return `This will return one movie with the id: ${movieId}`;
     }
 
     @Post()//post는 기본적으로 movie를 '생성'하기 위함
-    create(){
-        return "This will create a movie";
-    }
+    create(@Body() movieData){
+        console.log(movieData);
+        return movieData
+;    }
 
     @Delete('/:id')
     remove(@Param('id') movieId:string){
@@ -22,8 +29,13 @@ export class MoviesController {
     }
 
     // @Patch() 리소스의 일부만 업데이트
-    @Patch(':/id')
-    path(@Param('id') movieId: string){
-        return `This will path a movie withe the id: ${movieId}`;
+    // 우리가 보낼 데이터의 오브젝트를 리턴함
+    @Patch('/:id')
+    patch(@Param('id') movieId: string, @Body() updateData){
+        return {
+            updateData: movieId,
+            ...updateData,
+        };
     }
+
 }
